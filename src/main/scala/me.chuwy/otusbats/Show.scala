dee51b4c93548da3e52de6794d404dd6561af64b
asbalakireva <shurup4ik1@mail.ru>
2023-01-28 22:41:55 +0300

    hw Show and Monad

--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -8,21 +8,27 @@ trait Show[A] {
 object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
+  implicit val intShow: Show[Int] = fromJvm
+  implicit val stringShow: Show[String] = fromJvm
+  implicit val booleanShow: Show[Boolean] = fromJvm
 
 
   // 1.2 Instances with conditional implicit
-
   implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+   fromFunction((a: List[A]) => mkString_(a, "[", "]", ","))
+
+  implicit def setShow[A](implicit ev: Show[A]): Show[Set[A]] =
+    fromFunction((a: Set[A]) => mkString_(a, "[", "]", ","))
+
 
 
   // 2. Summoner (apply)
-
+  def apply[A](implicit ev: Show[A]): Show[A] = ev
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
-    def show(implicit ev: Show[A]): String =
-      ???
+    def show(implicit ev: Show[A]): String = ev.show(a)
+
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -38,17 +44,17 @@ object Show {
    *  @param separator. ',' in above example
    *  @param begin. '[' in above example
    *  @param end. ']' in above example
-   */
-  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
-
+//   */
+  def mkString_[A: Show](list: Iterable[A], begin: String, end: String, separator: String): String =
+    list.map(a => a.show).mkString(begin,separator,end)
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
-  
+  def fromJvm[A]: Show[A] = (a: A) => a.toString
+
+
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = (a: A) => f(a)
 
 }
